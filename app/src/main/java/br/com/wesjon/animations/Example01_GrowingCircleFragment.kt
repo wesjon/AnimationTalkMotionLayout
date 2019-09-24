7b4968b2a2090ab780f092b9a8d01ede472db0c7
Wesley Jonathan Marcolino <wesleyjonatan@gmail.com>
2019-09-24 00:09:14 -0300

    Add fab animation

--- app/src/main/java/br/com/wesjon/animations/Example01_GrowingCircleFragment.kt
+++ app/src/main/java/br/com/wesjon/animations/Example01_GrowingCircleFragment.kt
@@ -1,7 +1,26 @@
 package br.com.wesjon.animations
 
+import android.os.Bundle
+import android.view.View
+import android.widget.SeekBar
 import androidx.fragment.app.Fragment
 import br.com.wesjon.R
+import kotlinx.android.synthetic.main.fragment_example01_growing_square.*
 
-class Example01_GrowingCircleFragment: Fragment(R.layout.fragment_example01_growing_square) {
+class Example01_GrowingCircleFragment : Fragment(R.layout.fragment_example01_growing_square) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+//        seekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+//            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+//                motion_layout.progress = progress / 100f
+//            }
+//
+//            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+//            }
+//
+//            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+//            }
+//        })
+    }
 }
