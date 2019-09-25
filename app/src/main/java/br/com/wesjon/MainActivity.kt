c9ea294fadd1c16ddc12b4d6d7adc5c41cc934c2
Wesley Jonathan Marcolino <wesley.marcolino@concrete.com.br>
2019-09-25 15:19:42 -0300

    Add final fab animation

--- app/src/main/java/br/com/wesjon/MainActivity.kt
+++ app/src/main/java/br/com/wesjon/MainActivity.kt
@@ -2,6 +2,7 @@ package br.com.wesjon
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,17 +18,13 @@ class MainActivity : AppCompatActivity() {
         sample_list.adapter = AnimationSampleAdapter(
             object : AnimationSampleListener {
                 override fun onClick(animationSample: AnimationSamples) {
-                    startActivity(
-                        AnimationCanvasActivity.getIntent(
-                            this@MainActivity,
-                            animationSample
-                        )
-                    )
+                    startActivity(AnimationCanvasActivity.getIntent(this@MainActivity, animationSample))
                 }
             }
         ).apply {
             submitList(AnimationSamples.values().toList())
         }
+        sample_list.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
     }
 }
 
---
+++ app/src/main/java/br/com/wesjon/animations/MotionLayoutMovingSquareAnimationFragment.kt
@@ -0,0 +1,8 @@
+package br.com.wesjon.animations
+
+import androidx.fragment.app.Fragment
+import br.com.wesjon.R
+
+class MotionLayoutMovingSquareAnimationFragment : Fragment(R.layout.fragment_motion_layout_moving_square_animation) {
+
+}
